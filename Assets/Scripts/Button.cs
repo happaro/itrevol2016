2102df8c85642c315a57150c73fcb1eb330ad590
Dima Lupyak <dima.lupyak@gmail.com>
2016-11-12 14:47:09 +0200

    button

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,80 +2,88 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class Button : MonoBehaviour 
+public class Button : MonoBehaviour
 {
-	private bool isPressed;
+    private bool isPressed;
 
-	public delegate void MyAction();
-	public MyAction myAction;
+    public delegate void MyAction();
+    public MyAction myAction;
 
-	private Vector3 startScale;
-	//public Sprite enableSprite, disableSprite;
-	public bool pushingAction = false;
-	private bool isActive = true;
+    private Vector3 startScale;
+    //public Sprite enableSprite, disableSprite;
+    public bool pushingAction = false;
+    private bool isActive = true;
 
-	public enum ActionType
-	{
-		Menu,
-		Play,
-		Scan
-	}
-	public ActionType actionType;
+    public enum ActionType
+    {
+        Menu,
+        Play,
+        Scan,
+        SelectColor
+    }
+    public ActionType actionType;
 
-	void Awake()
-	{
-		startScale = transform.localScale;
-	}
+    void Awake()
+    {
+        startScale = transform.localScale;
+    }
 
-	void DownAction()
-	{
-		transform.localScale = startScale * 0.9f;
-	}
+    void DownAction()
+    {
+        transform.localScale = startScale * 0.9f;
+    }
 
-	void UpAction()
-	{
-		isPressed = false;
-		transform.localScale = startScale;
-	}
+    void UpAction()
+    {
+        isPressed = false;
+        transform.localScale = startScale;
+    }
 
-	void OnMouseDown()
-	{
-		DownAction ();
-		isPressed = true;
-	}
-		
-	void OnMouseExit()
-	{
-		UpAction ();
-		isPressed = false;	
-	}
+    void OnMouseDown()
+    {
+        DownAction();
+        isPressed = true;
+    }
 
-	void OnMouseUp()
-	{
-		if (isPressed) 
-		{
-			UpAction ();
-			Action ();
-		}
-	}
+    void OnMouseExit()
+    {
+        UpAction();
+        isPressed = false;
+    }
 
-	protected virtual void Action()
-	{
-		if (myAction == null) 
-		{
-			switch (actionType) 
-			{
-			case ActionType.Scan:
-				GameObject.FindObjectOfType<MultiObjectTrackingBasedOnColorSample> ().Scan();
-				break;
-			case ActionType.Menu:
-				SceneManager.LoadScene (0);
-				break;
-			case ActionType.Play:
-				SceneManager.LoadScene (1);
-				break;
-			}
-		}
+    void OnMouseUp()
+    {
+        if (isPressed)
+        {
+            UpAction();
+            Action();
+        }
+    }
 
-	}
+    protected virtual void Action()
+    {
+        if (myAction == null)
+        {
+            switch (actionType)
+            {
+                case ActionType.SelectColor:
+                    var target = GameObject.FindObjectOfType<TargetItem>();
+                    if (GetComponent<ColorItem>().name == target.name)
+                    {
+                        target.NewTarget();
+                    }
+                    break;
+                case ActionType.Scan:
+                    GameObject.FindObjectOfType<MultiObjectTrackingBasedOnColorSample>().Scan();
+                    break;
+                case ActionType.Menu:
+                    SceneManager.LoadScene(0);
+                    break;
+                case ActionType.Play:
+                    SceneManager.LoadScene(1);
+                    break;
+            }
+        }
+
+    }
 }
