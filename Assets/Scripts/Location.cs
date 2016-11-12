d18fe97adf8f9d067e2cf393ce0c14e4ab64fcb1
Happaro <happaro@gmail.com>
2016-11-12 12:47:07 +0200

    COMMITET

--- Assets/Scripts/Location.cs
+++ Assets/Scripts/Location.cs
@@ -1,22 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Location : MonoBehaviour {
-    public TextMesh text;
+    public TextMesh text, allowText;
     
+	public bool allowed = true;
+	public float allowDist = 0.00025f;
+	List<Vector2> usedPositions;
+	Vector2 lastPos;
 	void Start()
     {
+		usedPositions = new List<Vector2> ();
         StartCoroutine("Init");
     }
+
+	public void CheckPoint()
+	{
+		
+		allowed = false;
+		lastPos = new Vector2 (Input.location.lastData.latitude, Input.location.lastData.longitude);
+		//usedPositions.Add (new Vector2(Input.location.lastData.latitude, Input.location.lastData.longitude));
+		//allowText.text = "NOPE!";
+	}
 
 	public void UpdateLocation()
 	{
 		//text.text = string.Format("status:{5}\n x: {0}\\{1}\ny: {2}\\{3}\ntimestamp:{4}", Input.location.lastData.latitude, Input.location.lastData.longitude, Input.location.lastData.altitude, Input.location.lastData.horizontalAccuracy, Input.location.lastData.timestamp, Input.location.status);
 		//Debug.LogWarning (Input.location.lastData.ToString());
 		StartCoroutine("Init");
+
 	//	Input.location.
 	}
+
+	void UpdateAllow()
+	{
+		var distance = Vector2.Distance (new Vector2 (lastPos.x, lastPos.y), new Vector2 (Input.location.lastData.latitude, Input.location.lastData.longitude));
+		allowText.text = "Distance : " + string.Format ("{0:0.000000}", distance);
+		allowed = distance > allowDist;
+		/*foreach (var pos in usedPositions) 
+		{
+			var distance = Vector2.Distance (new Vector2 (pos.x, pos.y), new Vector2 (Input.location.lastData.latitude, Input.location.lastData.longitude));
+			string a = string.Format ("{0:0.000000}", distance);
+			allowText.text += "\n and " + a;
+			if (distance < allowDist)
+				{
+					allowed = false;
+					allowText.text += "NOT ALLOWED";
+					break;
+					
+				}
+		}	
+		if (allowed)
+			allowText.text += "ALLOWED";*/
+	}
+
 	void Update()
 	{
 		//if (Input.location.status == LocationServiceStatus.
@@ -35,7 +74,7 @@ public class Location : MonoBehaviour {
             
 
         // Start service before querying location
-        Input.location.Start();
+		Input.location.Start(1,1);
 
         // Wait until service initializes
         int maxWait = 60;
@@ -63,12 +102,14 @@ public class Location : MonoBehaviour {
         }
         else
         {
-			text.text = string.Format("lati: {0}\\long{1}\nalti: {2}\nacurracy:{3}\ntimestamp:{4}", Input.location.lastData.latitude, Input.location.lastData.longitude, Input.location.lastData.altitude, Input.location.lastData.horizontalAccuracy, Input.location.lastData.timestamp);
+			text.text = string.Format("status:{2}\nlati: {0}\nlong{1}", Input.location.lastData.latitude, Input.location.lastData.longitude, Input.location.status);
             //text.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
             // Access granted and location value could be retrieved
             Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+			//Input.location.
         }
 
+		UpdateAllow ();
         // Stop service if there is no need to query location updates continuously
         Input.location.Stop();
     }
